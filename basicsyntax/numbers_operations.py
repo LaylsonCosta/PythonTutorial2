92cc8e348e9cc8e8ca9fa5baeaf5afb72ed23989
lay <laylsonalmeida@gmail.com>
2018-03-28 16:19:37 -0300

    Treinamento #3

---
+++ basicsyntax/numbers_operations.py
@@ -0,0 +1,26 @@
+# One line comment
+
+# Exponentiation
+
+"""
+10^20
+"""
+exponents = 10 ** 20
+print(exponents)
+
+
+"""
+2^3
+"""
+exponents2 = 2 ** 3
+print(exponents2)
+
+"""
+MÓDULO
+Remainer, ou resto da divisão
+"""
+remainer = 100 % 3
+remainer2 = 100 % 2
+print(remainer)
+print(remainer2)
+
---
+++ basicsyntax/strings_methods.py
@@ -0,0 +1,36 @@
+"""
+Examples with string melhods in python
+"""
+
+# Accessing characteres in a string
+# index starts from zero
+
+first = "nyc" [0]
+city = "sfo"
+print(first)
+ft = city[0]
+print(ft)
+
+
+"""
+len()
+lower()
+upper()
+srt()
+"""
+
+str_variable = "This is a Mixed Case"
+
+print(str_variable)
+print(str_variable.lower())
+print(str_variable.upper())
+print(len(str_variable))
+
+print(str_variable + str(2))
+
+"""
+Concatenation
+"""
+
+print("Hello " +" "+ " World!!!")
+print("Hi" + " " + city)
