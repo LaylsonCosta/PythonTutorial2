58361cbaff07ee8dde434d798effa0b4ddec9dc7
lay <laylsonalmeida@gmail.com>
2018-03-28 14:07:51 -0300

    Treinamento dia #1

---
+++ basicsyntax/boolean.py
@@ -0,0 +1,22 @@
+"""
+Examples with boolean in python
+"""
+
+a = True
+b = False
+
+print(a)
+print(b)
+
+print("**************")
+print(bool(0))
+print(bool(1))
+print(bool(2))
+
+
+"""Empt and 0 value is False"""
+c = ""
+print(bool(c))
+
+c = "same value"
+print(bool(c))
---
+++ basicsyntax/strings.py
@@ -0,0 +1,22 @@
+"""
+Examples with strings in python
+"""
+
+
+a = "Double quote"
+b = 'Single quote'
+print(a)
+print(b)
+
+c = 'Need to use double "quote" inside a string'
+d = "Need to use single 'quote' inside a string"
+print(c)
+print(d)
+
+e = "Another way to use \"quotes\""
+f = "This is a single\
+ string"
+print(e)
+print(f)
+
+
