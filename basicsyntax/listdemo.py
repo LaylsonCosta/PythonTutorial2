6d5b70840a94ebdd3dfd12381d414562a5acc7a5
lay <laylsonalmeida@gmail.com>
2018-03-29 17:53:36 -0300

    Strings 29/03

---
+++ basicsyntax/listdemo.py
@@ -0,0 +1,29 @@
+"""
+Data tupe to store more than one value in one variable name
+List item are brackets, separeted with "," [ 1, 2, 3 ]
+"""
+
+
+cars = ["bmw", "honda", "audi"]
+empty_list = []
+
+print(empty_list)
+print(cars)
+
+print("#"*20)
+
+print(cars[0])
+
+num_list = [1, 2, 3]
+
+sum_num = num_list[0] + num_list[1]
+
+print(sum_num)
+
+
+more_cars = ["bmw", "honda", "audi"]
+print(more_cars[1])
+
+more_cars[1] = "Benz"
+print(more_cars[1])
+print(more_cars)
--- basicsyntax/strings_formatting.py
+++ basicsyntax/strings_formatting.py
@@ -1,8 +1,9 @@
 """
 Examples of string formatting
 """
-city = "nyc"
+city = "nyc abc 123"
 event = "show"
 
 print("Welcome to " + city + " and enjoy the event " + event)
 print("Welcome to %s and enjoy the event %s" % (city, event))
+print(city.split())
