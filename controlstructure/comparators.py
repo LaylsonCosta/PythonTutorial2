8d521dc216a664940f14fcf75bc4ffeed130a2ef
lay <laylsonalmeida@gmail.com>
2018-04-06 18:07:26 -0300

    Strings 29/03

--- controlstructure/comparators.py
+++ controlstructure/comparators.py
@@ -8,4 +8,14 @@
 """
 
 bool_one = 10 == 10
+not_equal = 10 != 10
+less_than = 10 < 10
+greater_than = 10 > 10
+lt_eq = 10 <= 10
+gt_eq = 10 >= 10
 print(bool_one)
+print(not_equal)
+print(less_than)
+print(greater_than)
+print(lt_eq)
+print(gt_eq)
