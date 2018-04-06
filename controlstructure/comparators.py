16d54d6b8f5097e78342069ee42320fa468d4418
lay <laylsonalmeida@gmail.com>
2018-04-06 18:02:28 -0300

    Strings 29/03

---
+++ controlstructure/comparators.py
@@ -0,0 +1,11 @@
+"""
+== --> value equality
+!= --> not equal to
+< --> less than
+> --> greater than
+<= --> less than or equal to
+>= --> greater than or equal to
+"""
+
+bool_one = 10 == 10
+print(bool_one)
